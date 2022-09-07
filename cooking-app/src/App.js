90a47cddfd559f8d1b2c5a317cac53d0a5f2f2d7
Marcin Eljasiewicz <m.eljasiewicz@gmail.com>
2022-09-07 08:32:00 +0200

    create react app and firebase

---
+++ cooking-app/src/App.js
@@ -0,0 +1,12 @@
+import { db } from './api/firebase';
+import './App.css';
+
+export const App = () => {
+  return (
+    <div className="App">
+     <h1>Cześć wszystkim!</h1>
+    </div>
+  );
+}
+
+
