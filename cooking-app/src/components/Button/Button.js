59aff61db45197ef9814f9d1fc209872be8ad8e2
Radosław Piotrowski <radek.rpiotrowski@gmail.com>
2022-09-14 15:37:38 +0200

    remove my own button implementation

--- cooking-app/src/components/Button/Button.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { StyledButton } from "./Button.styled";
-
-const Button = ({ children, primary }) => {
-  return <StyledButton>{children}</StyledButton>;
-};
-
-export default Button;
