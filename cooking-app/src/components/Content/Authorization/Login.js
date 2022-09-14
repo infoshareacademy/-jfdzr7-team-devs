03273986cc71927dd5ec411de82b42623fbf3185
Radosław Piotrowski <radek.rpiotrowski@gmail.com>
2022-09-14 15:06:28 +0200

    Login form styled with MUI, error toast

--- cooking-app/src/components/Content/Authorization/Login.js
+++ cooking-app/src/components/Content/Authorization/Login.js
@@ -1,33 +1,99 @@
-import Button from "../../Button/Button";
+import { Alert, Button, IconButton, Snackbar, TextField } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+
+import { signInWithEmailAndPassword } from "@firebase/auth";
+import React, { useState } from "react";
 import { StyledLogin } from "./Login.styled";
+import { auth } from "../../../api/firebase";
 
 export const Login = () => {
+  const defaultLoginForm = {
+    email: "",
+    password: "",
+  };
+
+  const [loginForm, setLoginForm] = useState(defaultLoginForm);
+  const [showAlert, setShowAlert] = useState(false);
+  const [responseMessage, setResponseMessage] = useState("");
+
+  const handleOpen = () => {
+    setShowAlert(true);
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason == "clickaway") {
+      return;
+    }
+
+    setShowAlert(false);
+    setResponseMessage("");
+  };
+
+  const updateLoginForm = (e) => {
+    setLoginForm({
+      ...loginForm,
+      [e.target.name]: e.target.value,
+    });
+
+    console.log(loginForm);
+  };
+
+  const loginUser = (e) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, loginForm.email, loginForm.password)
+      .then((jwt) => {
+        console.log(jwt);
+      })
+      .catch((e) => {
+        setResponseMessage(e.code);
+        setShowAlert(true);
+        console.dir(e);
+      });
+    setLoginForm(defaultLoginForm);
+  };
+
   return (
     <StyledLogin>
       <h1>Log in</h1>
 
-      <form>
-        <div className="form-control">
-          <input required type="text" name="login" />
-          <label htmlFor="">
-            <span>Email</span>
-          </label>
-        </div>
+      <form onSubmit={loginUser}>
+        <TextField
+          required
+          value={loginForm.email}
+          name="email"
+          label="email"
+          variant="filled"
+          onChange={updateLoginForm}
+        />
 
-        <div className="form-control">
-          <input required type="password" name="password" />
-          <label htmlFor="">
-            <span>Password</span>
-          </label>
-        </div>
+        <TextField
+          required
+          value={loginForm.password}
+          name="password"
+          label="password"
+          type="password"
+          variant="filled"
+          onChange={updateLoginForm}
+        />
 
-        <Button>Log in</Button>
+        <Button type="submit" variant="contained">
+          Login
+        </Button>
 
         <p>
           Don't have an account?
           <a href="#"> Register</a>
         </p>
       </form>
+
+      <Snackbar
+        open={showAlert}
+        autoHideDuration={2000}
+        onClose={handleClose}
+        message={responseMessage}
+      >
+        <Alert severity="warning">{responseMessage}</Alert>
+      </Snackbar>
     </StyledLogin>
   );
 };
--- cooking-app/src/components/Content/Authorization/Login.styled.js
+++ cooking-app/src/components/Content/Authorization/Login.styled.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const StyledLogin = styled.div`
   text-align: center;
-  background-color: steelblue;
+  // background-color: steelblue;
   max-width: 400px;
   margin: 0 auto;
   padding: 2rem;
@@ -15,46 +15,46 @@ export const StyledLogin = styled.div`
     margin: 2rem 0;
   }
 
-  .form-control {
-    position: relative;
-    margin: 20px 0 30px;
-    width: 300px;
-  }
+  // .form-control {
+  //   position: relative;
+  //   margin: 20px 0 30px;
+  //   width: 300px;
+  // }
 
-  .form-control input {
-    background-color: transparent;
-    border: 0;
-    border-bottom: 2px white solid;
-    display: block;
-    width: 100%;
-    font-size: 18px;
-    color: white;
-  }
+  // .form-control input {
+  //   background-color: transparent;
+  //   border: 0;
+  //   border-bottom: 2px white solid;
+  //   display: block;
+  //   width: 100%;
+  //   font-size: 18px;
+  //   color: white;
+  // }
 
-  .form-control input:focus,
-  .form-control input:valid {
-    outline: none;
-    border-bottom-color: lightblue;
-  }
+  // .form-control input:focus,
+  // .form-control input:valid {
+  //   outline: none;
+  //   border-bottom-color: lightblue;
+  // }
 
-  .form-control label {
-    position: absolute;
-    bottom: 2px;
-    left: 0;
-    pointer-events: none;
-  }
+  // .form-control label {
+  //   position: absolute;
+  //   bottom: 2px;
+  //   left: 0;
+  //   pointer-events: none;
+  // }
 
-  .form-control label span {
-    display: inline-block;
-    color: lightblue;
-    font-size: 18px;
-    min-width: 5px;
-    transition: 0.3s cubic-bezier(0.68, -0.55, 0.265, 1.55);
-  }
+  // .form-control label span {
+  //   display: inline-block;
+  //   color: lightblue;
+  //   font-size: 18px;
+  //   min-width: 5px;
+  //   transition: 0.3s cubic-bezier(0.68, -0.55, 0.265, 1.55);
+  // }
 
-  .form-control input:focus + label span,
-  .form-control input:valid + label span {
-    color: black;
-    transform: translateY(-25px);
-  }
+  // .form-control input:focus + label span,
+  // .form-control input:valid + label span {
+  //   color: black;
+  //   transform: translateY(-25px);
+  // }
 `;
