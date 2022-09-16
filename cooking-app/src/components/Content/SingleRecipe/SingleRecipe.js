ce55143514e6e100612d95dd7f8634c6745271d4
Agata Gryszkiewicz <gryszkiewicz.agata@gmail.com>
2022-09-16 12:00:44 +0200

    fix unknown id in SingleRecipe component, add alert

--- cooking-app/src/components/Content/SingleRecipe/SingleRecipe.js
+++ cooking-app/src/components/Content/SingleRecipe/SingleRecipe.js
@@ -4,40 +4,60 @@ import { onSnapshot } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { recipesCollection } from "../../../api/firebaseIndex";
 import { getDataFromSnapshot } from "../../../utils/GetDataFromSnapshot";
+import { ERROR_MESSAGE } from "../../../utils/ErrorMessageTexts";
+import Alert from "@mui/material/Alert";
 
 export const SingleRecipe = () => {
-  const [recipies, setRecipies] = useState([]);
-
+  const [recipes, setRecipes] = useState([]);
   const idCurrent = useParams();
 
   useEffect(() => {
     onSnapshot(recipesCollection, (singleRecipe) => {
-      setRecipies(getDataFromSnapshot(singleRecipe));
+      setRecipes(getDataFromSnapshot(singleRecipe));
     });
   }, []);
 
+  ///czy tutaj wydzielamy styl alertu na zewnatrz?
+  const ErrorMessage = () => {
+    return (
+      <Alert severity="error" variant="outlined">
+        {ERROR_MESSAGE.MISSING_WEBSITE}
+      </Alert>
+    );
+  };
+
+  const recipeFound = recipes.some((item) => {
+    if (item.id === idCurrent.id) {
+      return true;
+    } else return false;
+  });
+
   return (
     <>
-      {recipies.map((recipe) => {
-        if (recipe.id === idCurrent.id) {
-          return (
-            <div key={recipe.id}>
-              <img src={recipe.url} alt={`${recipe.title} image`} />
-              <PageTitle>{recipe.title}</PageTitle>
-              <p>
-                Categories:
-                {recipe.categories.map((category, index) => (
-                  <li key={index}>{category}</li>
-                ))}
-              </p>
-              <p>Time: {recipe.time}</p>
-              <p>Portions: {recipe.portion}</p>
-              <p>Ingredients: {recipe.ingredients}</p>
-              <p>How to prepare? {recipe.describe}</p>
-            </div>
-          );
-        }
-      })}
+      {!recipeFound ? (
+        <ErrorMessage />
+      ) : (
+        recipes.map((recipe) => {
+          if (recipe.id === idCurrent.id) {
+            return (
+              <div key={recipe.id}>
+                <img src={recipe.url} alt={`${recipe.title}`} />
+                <PageTitle>{recipe.title}</PageTitle>
+                <p>
+                  Categories:
+                  {recipe.categories.map((category, index) => (
+                    <li key={index}>{category}</li>
+                  ))}
+                </p>
+                <p>Time: {recipe.time}</p>
+                <p>Portions: {recipe.portion}</p>
+                <p>Ingredients: {recipe.ingredients}</p>
+                <p>How to prepare? {recipe.describe}</p>
+              </div>
+            );
+          }
+        })
+      )}
     </>
   );
 };
---
+++ cooking-app/src/utils/ErrorMessageTexts.js
@@ -0,0 +1,3 @@
+export const ERROR_MESSAGE = {
+    MISSING_WEBSITE: 'Sorry Chef, there is nothing here',
+  };
