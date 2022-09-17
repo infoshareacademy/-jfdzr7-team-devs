b5c2f2246f8d9b61c447b706268d4154eeaf2dfb
Marcin Eljasiewicz <m.eljasiewicz@gmail.com>
2022-09-17 08:54:24 +0200

    inputs changes

--- cooking-app/src/components/Content/ListRecipes/ListRecipes.js
+++ cooking-app/src/components/Content/ListRecipes/ListRecipes.js
@@ -1,24 +1,144 @@
-import { PageTitle } from "../../styles/Global.styled";
 import { onSnapshot } from "firebase/firestore";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { recipesCollection } from "../../../api/firebaseIndex";
+import { IndividualRecipe } from "./IndividualRecipe";
 import { getDataFromSnapshot } from "../../../utils/GetDataFromSnapshot";
+import { PageTitle } from "../../styles/Global.styled";
+import styled from "styled-components";
 
 export const ListRecipes = () => {
-  const [recipies, setRecipies] = useState([]);
+  const [datafromFirebase, setdatafromFirebase] = useState([]);
+  const [search, setSearch] = useState("");
+  const [salt, setSalt] = useState(false);
+  const [sweet, setSweet] = useState(false);
+  const [breakfast, setBreakfast] = useState(false);
+  const [dinner, setDinner] = useState(false);
+  const [dessert, setDessert] = useState(false);
+  const [lunch, setLunch] = useState(false);
+
+  const textInput = useRef();
+  const saltInput = useRef();
+  const sweetInput = useRef();
+  const breakfastInput = useRef();
+  const dinnerInput = useRef();
+  const dessertInput = useRef();
+  const lunchInput = useRef();
+
 
   useEffect(() => {
-    onSnapshot(recipesCollection, (singleRecipe) => {
-      setRecipies(getDataFromSnapshot(singleRecipe));
+    onSnapshot(recipesCollection, (snapshot) => {
+      setdatafromFirebase(getDataFromSnapshot(snapshot));
     });
   }, []);
 
+  const listofRecipe2 = datafromFirebase
+    .filter((item) => {
+      if (salt == true) {
+        return item.categories.includes("Salt");
+      } else if (sweet === true) {
+        return item.categories.includes("Sweet");
+      } else if (breakfast === true) {
+        return item.categories.includes("Breakfast");
+      } else if (dinner === true) {
+        return item.categories.includes("Dinner");
+      } else if (lunch === true) {
+        return item.categories.includes("Lunch");
+      } else if (dessert === true) {
+        return item.categories.includes("Dessert");
+      } else if (search.toLowerCase() === "") {
+        return item;
+      } else return item.title.toLowerCase().includes(search);
+    })
+    .map((singleRecipe) => {
+      return (
+        <IndividualRecipe singleRecipe={singleRecipe}/>
+      );
+    });
+
+
   return (
     <>
-      <PageTitle>Recipes</PageTitle>
-      {recipies.map(({ id, title }) => (
-        <li key={id}>{title}</li>
-      ))}
+    <PageTitle>Recipes</PageTitle>
+    <label htmlFor="filter">Search by recipe title </label>
+    <input
+    id="filter"
+        ref={textInput}
+        type="text"
+        onChange={(e) => {
+          setSearch(e.target.value);
+        }}
+      />
+      <br/>
+
+      <label htmlFor="salt">Salt</label>
+      <input
+        ref={saltInput}
+        name="salt"
+        type="checkbox"
+        onChange={() => {
+          setSalt(!salt);
+        }}
+      />
+
+      <label htmlFor="sweet">Sweet</label>
+      <input
+        ref={sweetInput}
+        name="sweet"
+        type="checkbox"
+        onChange={() => {
+          setSweet(!sweet);
+        }}
+      />
+
+      <label htmlFor="dinner">Dinner</label>
+      <input
+        ref={dinnerInput}
+        name="dinner"
+        type="checkbox"
+        onChange={() => {
+          setDinner(!dinner);
+        }}
+      />
+
+<label htmlFor="lunch">Lunch</label>
+      <input
+        ref={lunchInput}
+        name="breakfast"
+        type="checkbox"
+        onChange={() => {
+          setLunch(!lunch);
+        }}
+      />
+
+      <label htmlFor="breakfast">Breakfast</label>
+      <input
+        ref={breakfastInput}
+        name="breakfast"
+        type="checkbox"
+        onChange={() => {
+          setBreakfast(!breakfast);
+        }}
+      />
+
+<label htmlFor="dessert">Dessert</label>
+      <input
+        ref={dessertInput}
+        name="dessert"
+        type="checkbox"
+        onChange={() => {
+          setDessert(!dessert);
+        }}
+      />
+
+
+      <StyledDiv>{listofRecipe2}</StyledDiv>
     </>
   );
 };
+
+const StyledDiv = styled.div`
+display: flex;
+flex-direction: row;
+flex-wrap: wrap;
+`
+
--- cooking-app/src/components/Content/ListRecipes/RecipeBreakfast.js
+++ cooking-app/src/components/Content/ListRecipes/RecipeBreakfast.js
@@ -1,9 +1,109 @@
 import { PageTitle } from "../../styles/Global.styled";
+import { onSnapshot } from "firebase/firestore";
+import { recipesCollection } from "../../../api/firebaseIndex";
+import { useEffect, useReducer, useState } from "react";
+import { getDataFromSnapshot } from "../../../utils/GetDataFromSnapshot";
+import styled from "styled-components";
+import { IndividualRecipe } from "./IndividualRecipe";
+
+const reducer = (currentState, action)=>{
+switch(action.type){
+  case "salt":
+    return {saltInput: !currentState.saltInput};
+  case "sweet":
+    return {sweetInput: !currentState.sweetInput};
+  case "text":
+    return {filterInput: action.payload};
+  default: return currentState
+}}
 
 export const RecipeBreakfast = () => {
+  const [datafromFirebase, setdatafromFirebase] = useState([]);
+
+  const [state, dispacher] = useReducer(reducer, {saltInput: false, sweetInput: false, filterInput: ""});
+
+  const filterFunc = (e)=>{
+    dispacher({type: "text", payload: e.target.value})
+  }
+
+  const saltFunc = ()=>{
+  dispacher({type: "salt"})
+}
+
+const sweetFunc = ()=>{
+  dispacher({type: "sweet"})
+}
+
+console.log(state.filterInput);
+console.log(state.saltInput);
+console.log(state.sweetInput);
+
+  useEffect(() => {
+    onSnapshot(recipesCollection, (snapshot) => {
+      setdatafromFirebase(getDataFromSnapshot(snapshot));
+    });
+  }, []);
+
+  const listofRecipe2 = datafromFirebase
+    .filter((item) => {
+        return item.categories.includes("Breakfast");
+    })
+    .filter((item) => {
+      if (state.saltInput == true) {
+        return item.categories.includes("Salt");
+      } else if (state.sweetInput === true) {
+        return item.categories.includes("Sweet");
+      } else if (state.filterInput !== "") {
+        return item.title.includes(state.filterInput)
+      } else return item
+    })
+    .map((singleRecipe) => {
+      return (
+        <IndividualRecipe key={singleRecipe.id} singleRecipe={singleRecipe}/>
+      );
+    });
+
   return (
     <>
       <PageTitle>Breakfast</PageTitle>
+
+      <label htmlFor="filter">Search by recipe title </label>
+    <input
+    id="filter"
+        type="text"
+        onChange={filterFunc} // payload
+        value={state.filterInput}
+      />
+      <br/>
+
+      <label htmlFor="salt">Salt</label>
+      <input
+        id="salt"
+        type="checkbox"
+        onChange={saltFunc}
+        checked={state.saltInput}
+        value={state.saltInput}
+      />
+
+      <label htmlFor="sweet">Sweet</label>
+      <input
+        id="sweet"
+        type="checkbox"
+        onChange={sweetFunc}
+        checked={state.sweetInput}
+        value={state.sweetInput}
+      />
+
+      <StyledDiv>{listofRecipe2}</StyledDiv>
+
+
+
     </>
   );
 };
+
+const StyledDiv = styled.div`
+display: flex;
+flex-direction: row;
+flex-wrap: wrap;
+`
