84118addd102a95c2d86430537a77052d8d080a9
magdalena-rozalewicz <magdalena.rozalewicz@gmail.com>
2022-09-19 11:03:36 +0200

    imgRef- references moved outside of the function uploadImage

--- cooking-app/src/components/Content/AddRecipes/SelectImageToUpload.js
+++ cooking-app/src/components/Content/AddRecipes/SelectImageToUpload.js
@@ -1,6 +1,4 @@
-import { StyledInputFile } from "./SelectImageUpload.styled";
 import * as React from "react";
-import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import PhotoCamera from "@mui/icons-material/PhotoCamera";
 import Stack from "@mui/material/Stack";
