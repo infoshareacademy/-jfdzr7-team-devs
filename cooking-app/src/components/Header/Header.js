db455073733a117348cdae18e3501613f9df87d7
Radosław Piotrowski <radek.rpiotrowski@gmail.com>
2022-09-29 18:53:17 +0200

    remove unused import and variables across the project

--- cooking-app/src/components/Header/Header.js
+++ cooking-app/src/components/Header/Header.js
@@ -3,22 +3,13 @@ import {
   StyledHeaderContent,
   NavButton,
   NavButtonLogo,
-  NavIcon,
 } from "./Header.styled";
 import { Container } from "../styles/Global.styled";
 import { FaSearch } from "react-icons/fa";
 import { MenuListDropdown } from "./MenuListDropdown";
-import { Avatar, Button, IconButton } from "@mui/material";
-import { signOut } from "firebase/auth";
-import { auth } from "../../api/firebase";
-import { useContext } from "react";
-import { UserDataContext } from "../../App";
-import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import AccountMenu from "./AccountMenu";
 
 export const Header = ({ isLoggedIn }) => {
-  const userData = useContext(UserDataContext);
-
   return (
     <Container>
       <StyledHeader>
